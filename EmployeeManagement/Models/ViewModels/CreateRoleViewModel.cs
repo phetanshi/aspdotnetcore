68f5bcdb96870ffe482abdd0115624410e57781f
Padmasekhar <padmasekhar.pottepalem@gmail.com>
2021-06-13 12:05:06 +0530

    Added Roles Creating Page

---
+++ EmployeeManagement/Models/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models.ViewModels
+{
+    public class CreateRoleViewModel : IViewModel
+    {
+        public string Message { get; set; }
+        [Required]
+        [Display(Name = "Role")]
+        public string RoleName { get; set; }
+    }
+}
