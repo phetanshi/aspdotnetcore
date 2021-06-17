c65f71555609c256edfa1df9686bc5bd723b5553
Padmasekhar <padmasekhar.pottepalem@gmail.com>
2021-06-17 07:31:55 +0530

    Delete Role Functionality added

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -293,6 +293,35 @@ namespace EmployeeManagement.Controllers
                 return RedirectToAction("ListUsers", users);
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                var roles = roleManager.Roles;
+                return RedirectToAction("ListRoles", roles);
+            }
+        }
+
         [HttpGet]
         [AllowAnonymous]
         public IActionResult AccessDenied()
