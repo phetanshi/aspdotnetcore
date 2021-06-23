73249f0d3a0d002bdac273262a541568311e862c
Padmasekhar <padmasekhar.pottepalem@gmail.com>
2021-06-23 11:04:51 +0530

    n/a

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -49,9 +49,8 @@ namespace EmployeeManagement
             });
 
             services.AddAuthorization(options => {
-                options.AddPolicy("DeleteRolePolicy", 
-                    policy => policy.RequireClaim("Delete Role")
-                                    .RequireClaim("Create Role"));
+                options.AddPolicy("DeleteRolePolicy", policy => policy.RequireClaim("Delete Role"));
+                options.AddPolicy("AdminRolePolicy", policy => policy.RequireClaim("Admin"));
             });
 
             //services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
